af9a72eb0660be46e8b1d6e1dc0a8ece47d387c2
hirarki wendri <ventus.kiki@gmail.com>
2022-06-24 16:52:17 +0700

    adding agents constant

---
+++ lib/app/constants/agents_constant.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_dicoding_submission/app/data/models/agent.dart';
+
+final List<Agent> kAgents = [
+  Agent(
+    name: "Phoenix",
+    score: 3.0,
+    image:
+        "https://i.pinimg.com/originals/ef/ef/05/efef0506561ae74d0e66cd4a14971c8f.jpg",
+  ),
+  Agent(
+    name: "Sage",
+    score: 4.0,
+    image:
+        "https://w0.peakpx.com/wallpaper/673/210/HD-wallpaper-video-game-valorant-sage-valorant.jpg",
+  ),
+  Agent(
+    name: "Brimstone",
+    score: 4.0,
+    image:
+        "http://cdn.gamer-network.net/2020/metabomb/valorant-how-to-play-brimstone.jpg",
+  ),
+  Agent(
+    name: "Yoru",
+    score: 4.0,
+    image: "https://pbs.twimg.com/media/ErURWLFXMAAAqHU.jpg",
+  ),
+  Agent(
+    name: "Fade",
+    score: 5.0,
+    image:
+        "https://id.jugomobile.com/wp-content/uploads/2022/04/Operator-Valorant-baru-Fade-memiliki-kemampuan-seram-yang-ditampilkan-di.jpg",
+  ),
+  Agent(
+      name: "Skye",
+      score: 3.5,
+      image:
+          "https://reshape.sport1.de/c/t/1a73bc71-8b6c-47a7-bc15-f8d54ffeec87/1200x900"),
+  Agent(
+    name: "Sova",
+    score: 2.0,
+    image:
+        "https://cdn.oneesports.gg/cdn-data/2022/03/Valorant_Sova_Cinematic.jpg",
+  ),
+  Agent(
+    name: "Chamber",
+    score: 3.0,
+    image:
+        "https://cdn.upstation.asia/wp-content/uploads/2021/11/01150710/chamber-valorant-banner-800x445.jpg",
+  ),
+  Agent(
+    name: "Omen",
+    score: 4.0,
+    image:
+        "https://dailyspin.id/wp-content/uploads/2020/10/agen-omen-Valorant-700x394.jpg",
+  ),
+  Agent(
+    name: "Astra",
+    score: 3.0,
+    image:
+        "https://i2.wp.com/thegamehaus.com/wp-content/uploads/2021/02/Astra-Valorant.png?fit=1119%2C1024&ssl=1",
+  ),
+];
